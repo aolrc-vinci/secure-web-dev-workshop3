3fdc5f6bbeb1742b516ba489fd1cb744b11b0cb1
aolrc-vinci <113455524+aolrc-vinci@users.noreply.github.com>
2022-11-23 21:04:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const locationController = require('./locations/locations.controller')
 const app = express()
 const port = 3000
 
+require('dotenv').config()
+
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGO_URI).then(()=> console.log("Connected")).catch((error) => console.log("Error"))
+
+
 
 app.use(locationController)
 
 app.listen(port, () => {
 	console.log(`API listening on port ${port}, visit http://localhost:${port}/`)
 })
+
+mongoose.connection.close();
