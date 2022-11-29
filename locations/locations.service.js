ee8050a1649c45db9698dc8a755029700cb35b6c
aolrc-vinci <113455524+aolrc-vinci@users.noreply.github.com>
2022-11-29 19:08:56 +0100

    Update locations.service.js

--- locations/locations.service.js
+++ locations/locations.service.js
@@ -3,7 +3,32 @@
 const Location = require('./locations.model')
 
 function findAll () {
-	return [1,2,3,4]
+	return Location.find({}).limit(10).lean()
+
+}
+
+function findOne (id){
+	return Location.findById(id)
+}
+
+async function Create(body){
+	const location = new Location(body)
+	await location.save()
+	return location
+}
+
+async function eraseOne(id){
+	await Location.deleteOne({_id : id})
+	return "erased !"
+}
+
+async function Patch(body){
+	await Location.findByIdAndUpdate({_id : body}, body)
+	return findOne(body._id)
 }
 
 module.exports.findAll = findAll
+module.exports.findOne = findOne
+module.exports.eraseOne = eraseOne
+module.exports.Create = Create
+module.exports.Patch = Patch
