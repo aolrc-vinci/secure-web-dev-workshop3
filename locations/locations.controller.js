72ceb1fba0583716400e572f87eb5648994e461c
aolrc-vinci <113455524+aolrc-vinci@users.noreply.github.com>
2022-11-29 19:08:52 +0100

    Update locations.controller.js

--- locations/locations.controller.js
+++ locations/locations.controller.js
@@ -4,9 +4,29 @@
 const router = require('express').Router()
 const locationsService = require('./locations.service')
 
-router.get('/locations', (req, res) => {
-	return res.status(200).send({locations: []})
+
+router.get('/locations', async (req, res) => {
+	return res.status(200).send({locations: await locationsService.findAll()})
 })
+
+router.get('/locations/:id', async (req, res) => {
+	return res.status(200).send({location: await locationsService.findOne(req.params.id)})
+})
+
+router.delete('/locations/:id', async (req, res) => {
+	return res.status(200).send({params: await locationsService.eraseOne(req.params.id)})
+})
+
+router.post('/locations/', async (req, res) => {
+	console.log(req.body)
+	return res.status(200).send({location: await locationsService.Create(req.body)})
+})
+
+router.patch('/locations/', async (req, res) => {
+	console.log(req.body)
+	return res.status(200).send({location: await locationsService.Patch(req.body)})
+})
+
 
 
 module.exports = router
